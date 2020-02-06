47ab5539c0c099b86e7b0fe26dcbea45b6034892
simonezanfagna <zanfagnasimone@live.it>
2020-02-06 18:59:24 +0100

    fine.

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $dipendente1 = new Dipendente('Mario', 'Rossi', '437891', 'operaio');
 // calcolo stipendio annuo inserendo la paga mensile
 
 try {
-  $dipendente1->stipendioAnnuo(1000);
+  $dipendente1->stipendioAnnuo(2000);
 } catch(Exception $e) {
   echo $e->getMessage();
 }
